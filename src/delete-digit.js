697871bd7602ec74e3f237b1f461962a1cfb4318
User <gorbatovskiy.igor@gmail.com>
2023-10-23 18:26:43 +0300

    feat: finish 7 tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  n = n.toString();
+  let firstHalf = "";
+  let secondHalf = "";
+  let result = [];
+  let resultStr = "";
+  for (let i = 0; i < n.length; i++) {
+    if (i === 0) {
+      resultStr = n.substring(1, n.length);
+      result.push(+resultStr);
+    } else {
+      firstHalf = n.substring(0, i);
+      secondHalf = n.substring(i + 1, n.length);
+      resultStr = firstHalf + secondHalf;
+      result.push(+resultStr);
+    }
+  }
+  result = result.sort((a, b) => b - a);
+  return result[0];
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let counter = 0;
+  let prevSymbol = "";
+  let result = "";
+  str = str.split("");
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char !== prevSymbol) {
+      if (counter > 1) {
+        result = `${result}${counter}`;
+      }
+      result = `${result}${prevSymbol}`;
+      counter = 1;
+    } else {
+      counter++;
+    }
+    prevSymbol = char;
+  }
+  if (counter > 1) {
+    result = `${result}${counter}`;
+  }
+  result = `${result}${prevSymbol}`;
+
+  return result;
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
